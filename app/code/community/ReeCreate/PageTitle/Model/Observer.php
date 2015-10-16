5049f567868a2f8f3d207c40d0103c6f9052085d
Rees Clissold <rees@reecreate.com>
2015-10-16 13:58:17 +0100

    Rewrite Category Page Title
    Rewrites the titles on the category pages. This fixes #3 and lays the
    foundations for #4.

--- app/code/community/ReeCreate/PageTitle/Model/Observer.php
+++ app/code/community/ReeCreate/PageTitle/Model/Observer.php
@@ -16,4 +16,19 @@ class ReeCreate_PageTitle_Model_Observer
         }
         return $this;
     }
+
+    /**
+     * Change category meta title on category view
+     *
+     * @pram Varien_Event_Observer $observer
+     * @return ReeCreate_PageTitle_Model_Observer
+     */
+    public function controller_action_layout_generate_blocks_after(Varien_Event_Observer $observer)
+    {
+        if (Mage::registry('current_category')) {
+            $head = $observer->getLayout()->getBlock('head');
+            $title = Mage::registry('current_category')->getName();
+            $head->setTitle($title);
+        }
+    }
 }
